92f6e6fe90cda788ef246050f861e9b764e9641b
Niklas Schoenberger <nik.schoe@icloud.com>
2018-02-16 17:26:43 +0100

    page fix v.17

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-define("APP_ROOT", "http://xschoenberger.com/");
+define("APP_ROOT", "http://localhost:8888/x_schoenberger/");
 
 include 'includes/dbconnect.php';
 include 'includes/functions.php';
@@ -22,7 +22,7 @@ $page = (isset($_GET['page'])) ? $_GET['page'] : "home";
 $page = rtrim($page, "/");
 $page = explode("/", $page);
 
-
+var_dump($page);
 
 if ($page[0] == "work" && !isset($page[1])) {
     header("Location:" . APP_ROOT . 'work/photography');
@@ -41,9 +41,9 @@ if ($page[0] == "backend") {
 
 $content .= "$page[0].php";
 
-/* if (isset($page[0]) && !file_exists("content/$page[0].php")) {
+if (isset($page[0]) && !file_exists("content/$page[0].php")) {
     $content = "content/error.php";
-} */
+}
 
 
 include "header.php";
