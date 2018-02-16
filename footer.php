a68789d76d0cb04b21b3b8490341947bd8709dd9
Niklas Schoenberger <nik.schoe@icloud.com>
2018-02-16 22:54:06 +0100

    responsive --fix v.001

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
     <script src="<?php echo APP_ROOT ?>assets/js/main.js"></script>
     <script src="<?php echo APP_ROOT ?>assets/js/home.js"></script>
 <?php } elseif ($page_pri == "slider") { ?>
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>;
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script src="<?php echo APP_ROOT ?>assets/js/TweenMax.min.js"></script>
     <script src="<?php echo APP_ROOT ?>assets/js/slider.js"></script>
 <?php } elseif ($page_sec == "photography" && isset($page_ter)) { ?>
