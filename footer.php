50bb8cfd4fcbb14c8019226404d61062c420e04c
Niklas Schoenberger <nik.schoe@icloud.com>
2018-02-17 18:43:28 +0100

    files --minified v.002

--- footer.php
+++ footer.php
@@ -5,20 +5,20 @@
 
 <?php if ($page_pri === "home") { ?>
     <script src="<?php echo APP_ROOT ?>assets/js/TweenMax.min.js"></script>
-    <script src="<?php echo APP_ROOT ?>assets/js/main.js"></script>
-    <script src="<?php echo APP_ROOT ?>assets/js/home.js"></script>
+    <script src="<?php echo APP_ROOT ?>assets/js/min/main.js"></script>
+    <script src="<?php echo APP_ROOT ?>assets/js/min/home.js"></script>
 <?php } elseif ($page_pri == "slider") { ?>
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script src="<?php echo APP_ROOT ?>assets/js/TweenMax.min.js"></script>
-    <script src="<?php echo APP_ROOT ?>assets/js/slider.js"></script>
+    <script src="<?php echo APP_ROOT ?>assets/js/min/slider.js"></script>
 <?php } elseif ($page_sec == "photography" && isset($page_ter)) { ?>
     <script src="<?php echo APP_ROOT ?>assets/js/TweenMax.min.js"></script>
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-    <script src="<?php echo APP_ROOT ?>assets/js/slider.js"></script>
-    <script src="<?php echo APP_ROOT ?>assets/js/main.js"></script>
+    <script src="<?php echo APP_ROOT ?>assets/js/min/slider.js"></script>
+    <script src="<?php echo APP_ROOT ?>assets/js/min/main.js"></script>
 <?php } else { ?>
     <script src="<?php echo APP_ROOT ?>assets/js/TweenMax.min.js"></script>
-    <script src="<?php echo APP_ROOT ?>assets/js/main.js"></script>
+    <script src="<?php echo APP_ROOT ?>assets/js/min/main.js"></script>
 <?php } ?>
 
 </body>
