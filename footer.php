dd5f1b3c98b2c79fceba5036054525a3999d3430
Niklas Schoenberger <nik.schoe@icloud.com>
2018-02-18 14:00:08 +0100

    footer--updated v.002

--- footer.php
+++ footer.php
@@ -3,24 +3,25 @@
     <span>© 2017 | Niklas Schoenberger</span>
 </footer>
 
-<?php if ($page_pri === "home") { ?>
-    <script src="<?php echo APP_ROOT ?>assets/js/TweenMax.min.js"></script>
-    <!-- <script src="<?php echo APP_ROOT ?>assets/js/min/main.js"></script> -->
-    <?php echo "<script src='" . APP_ROOT . "assets/js/min/main.js'></script>" ?>
-    <script src="<?php echo APP_ROOT ?>assets/js/min/home.js"></script>
-<?php } elseif ($page_pri == "slider") { ?>
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-    <script src="<?php echo APP_ROOT ?>assets/js/TweenMax.min.js"></script>
-    <script src="<?php echo APP_ROOT ?>assets/js/min/slider.js"></script>
-<?php } elseif ($page_sec == "photography" && isset($page_ter)) { ?>
-    <script src="<?php echo APP_ROOT ?>assets/js/TweenMax.min.js"></script>
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-    <script src="<?php echo APP_ROOT ?>assets/js/min/slider.js"></script>
-    <script src="<?php echo APP_ROOT ?>assets/js/min/main.js"></script>
-<?php } else { ?>
-    <script src="<?php echo APP_ROOT ?>assets/js/TweenMax.min.js"></script>
-    <script src="<?php echo APP_ROOT ?>assets/js/min/main.js"></script>
-<?php } ?>
+<?php
+if ($page_pri === "home") {
+    echo "<script src='" . APP_ROOT . "assets/js/TweenMax.min.js'></script>";
+    echo "<script src='" . APP_ROOT . "assets/js/min/main.js'></script>";
+    echo "<script src='" . APP_ROOT . "assets/js/min/home.js'></script>";
+} elseif ($page_pri == "slider") {
+    echo "<script src=\"https://code.jquery.com/jquery-2.2.4.min.js\" integrity=\"sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=\" crossorigin=\"anonymous\"></script>";
+    echo "<script src='" . APP_ROOT . "assets/js/TweenMax.min.js'></script>";
+    echo "<script src='" . APP_ROOT . "assets/js/min/slider.js'></script>";
+} elseif ($page_sec == "photography" && isset($page_ter)) {
+    echo "<script src='" . APP_ROOT . "assets/js/TweenMax.min.js'></script>";
+    echo "<script src=\"https://code.jquery.com/jquery-2.2.4.min.js\" integrity=\"sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=\" crossorigin=\"anonymous\"></script>";
+    echo "<script src='" . APP_ROOT . "assets/js/min/slider.js'></script>";
+    echo "<script src='" . APP_ROOT . "assets/js/min/main.js'></script>";
+} else {
+    echo "<script src='" . APP_ROOT . "assets/js/TweenMax.min.js'></script>";
+    echo "<script src='" . APP_ROOT . "assets/js/min/main.js'></script>";
+}
+?>
 
 </body>
 </html>
