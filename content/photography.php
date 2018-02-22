560df5e9905c9c127eb841734db1c09b64669c8c
Niklas Schoenberger <nik.schoe@icloud.com>
2018-02-22 21:08:10 +0100

    photography --update v.001

--- content/photography.php
+++ content/photography.php
@@ -32,6 +32,8 @@
         $albums[$fin_href] = "1";
 
         if (!isset($page_ter) && $page_pri == "work") {
+
+
             ?>
             <div class="content__photo__cnt">
                 <h2 class="key-title"><?php echo $keyword; ?></h2>
@@ -46,29 +48,90 @@
             $categories = array();
             $rows = array();
             $count = 0;
+
             foreach ($key_row as $key => $img) {
                 $count++;
+
                 if ($page_ter == str_replace(" ", "_", strtolower(str_replace(".", "-", $img['album'])))) {
                     $categories[$img['keywords']] = $count;
                     array_push($rows, $img);
+
                 }
             };
 
+
             $count__ = 0;
+
+            $grid_count = [];
+
+            $grid_key = 0;
+            $grid_prevkey = count($categories);
+            foreach ($categories as $key => $images) {
+                $grid_key++;
+                $grid_count[$grid_key] = $images;
+
+            }
+
+
+            $calc_grid = [];
+            for($i = 1; $i < count($grid_count)+1; $i++) {
+
+
+                if($i == 1){
+                    $calc_grid[$i] = $grid_count[$i];
+                } else {
+                    $calc_grid[$i] = $grid_count[$i] - $grid_count[$i - 1];
+                }
+            }
+
+            //var_dump($calc_grid);
+
+
+
             foreach ($categories as $categ => $idx) {
                 $count__++;
+
+
+                //var_dump($img_count);
                 ?>
                 <div class="content__photo__keyword gallery__cnt">
                     <h2 class="title"><em><?php echo substr($categ, 0, 4); ?></em><span><?php echo $categ; ?></span>
                     </h2>
                     <?php
+                    $xx = 0;
+
                     foreach ($rows as $key => $row):
+
+
+                        $file = getimagesize($row['path']);
+                        $format = "";
+
+                        //var_dump($file);
+
+                        if ($file[0] > $file[1]) {
+                            $format = "landscape";
+                        }
+                        if ($file[1] > $file[0]) {
+                            $format = "portrait";
+                        }
+
+                        //var_dump($format);
+
                         if ($row['keywords'] == $categ) {
+                            $xx++;
+                            $unxx = 0;
+
+
                             $check_lines = explode(".jpg", $row['path']);
                             $path = APP_ROOT . $check_lines[0] . '_lines.png';
                             $link = str_replace("photo/", "photo/big/", $row['path']);
                             $link = APP_ROOT . $link;
-                            echo "<div class='img gallery__cnt__thmb'>";
+                            if($xx % 2 == 0) {
+                                $unxx = $xx - 1;
+                                echo "<div class='img gallery__cnt__thmb {$format} col-{$unxx}'>";
+                            } else {
+                                echo "<div class='img gallery__cnt__thmb {$format} col-{$xx}'>";
+                            }
                             echo "<a data-idx='{$key}' href=\"$link\">";
                             echo "<img src='" . APP_ROOT . $row['path'] . "'>";
                             if (file_exists($check_lines[0] . "_lines.png")) {
